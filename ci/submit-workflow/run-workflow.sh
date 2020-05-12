596620cb6ce59f5505668d15f937294df34d83a8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-05-12 23:48:47 +0900

    ci: setup CircleCI

--- ci/submit-workflow/run-workflow.sh
+++ ci/submit-workflow/run-workflow.sh
@@ -8,6 +8,10 @@ cd "$(dirname "$0/")/../.."
 workflow="$1"
 parameter="$2"
 
+if [ -n "${ARGO_TOKEN:-}" ]; then
+  alias argo='argo --token "$ARGO_TOKEN"'
+fi
+
 name=$(argo submit "$workflow" --parameter-file "$parameter" -o json | jq -r ".metadata.name")
 trap 'argo terminate "$name"' INT TERM
 argo logs --follow --timestamps "$name"
